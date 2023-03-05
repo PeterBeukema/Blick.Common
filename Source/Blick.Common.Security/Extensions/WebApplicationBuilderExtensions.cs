563c6ad67b3e21d56c67682aca29bfb40be90cf0
Peter Beukema <pjdbeukema@gmail.com>
2023-03-05 14:04:06 +0100

    Added extension method

---
+++ Source/Blick.Common.Security/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Blick.Common.Security.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddSecurity(this WebApplicationBuilder builder)
+        => builder.Services.AddSecurity(builder.Configuration);
+}
